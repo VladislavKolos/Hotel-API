70b34bdddfd3f897adbc587b11a053f696e49322
vladislav <vladislav.kolos.y@gmail.com>
2025-03-01 18:35:02 +0300

    001 fix after review

---
+++ src/main/java/org/example/api/hotel/util/HotelApiConstantUtil.java
@@ -0,0 +1,12 @@
+package org.example.api.hotel.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class HotelApiConstantUtil {
+    public static final String NAME = "name";
+    public static final String CITY = "city";
+    public static final String BRAND = "brand";
+    public static final String ADDRESS = "address";
+    public static final String COUNTRY = "country";
+}
